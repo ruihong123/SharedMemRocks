6f19703ca0d3cfdec8bfeff93bbf59829f50dc6d
ruihong123 <wang4996@purdue.edu>
2020-10-18 00:05:44 -0400

    debug file system

--- env/rdma.cc
+++ env/rdma.cc
@@ -1218,11 +1218,11 @@ void RDMA_Manager::Allocate_Local_RDMA_Slot(ibv_mr*& mr_input,
 bool RDMA_Manager::Deallocate_Local_RDMA_Slot(ibv_mr* mr, ibv_mr* map_pointer) const {
   int buff_offset = static_cast<char*>(mr->addr) - static_cast<char*>(map_pointer->addr);
   assert(buff_offset%Block_Size == 0);
-  return Local_Mem_Bitmap->at(map_pointer).deallocate_memory_slot(buff_offset);
+  return Local_Mem_Bitmap->at(map_pointer).deallocate_memory_slot(buff_offset/Block_Size);
 }
 bool RDMA_Manager::Deallocate_Remote_RDMA_Slot(SST_Metadata* sst_meta) const {
   int buff_offset = static_cast<char*>(sst_meta->mr->addr) - static_cast<char*>(sst_meta->map_pointer->addr);
   assert(buff_offset%Table_Size == 0);
-  return Remote_Mem_Bitmap->at(sst_meta->map_pointer).deallocate_memory_slot(buff_offset);
+  return Remote_Mem_Bitmap->at(sst_meta->map_pointer).deallocate_memory_slot(buff_offset/Block_Size);
 }
 
