521597ff036788fcf0711431c727dc3081b3d578
ruihong123 <wang4996@purdue.edu>
2020-10-25 12:07:32 -0400

    commit multi qp

---
+++ compute_node.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include "rocksdb/rdma.h"
+
+void client_thread(RDMA_Manager* rdma_manager){
+
+  auto myid = std::this_thread::get_id();
+  std::stringstream ss;
+  ss << myid;
+  std::string thread_id = ss.str();
+  rdma_manager->Remote_Memory_Register(1024*1024*1024);
+  rdma_manager->Remote_Query_Pair_Connection(thread_id);
+  std::cout << rdma_manager->remote_mem_pool[0];
+  ibv_mr mem_pool_table[2];
+  mem_pool_table[0] = *(rdma_manager->local_mem_pool[0]);
+  mem_pool_table[1] = *(rdma_manager->local_mem_pool[0]);
+  mem_pool_table[1].addr = (void*)((char*)mem_pool_table[1].addr + sizeof("message from computing node"));// PROBLEM Could be here.
+
+  char *msg = static_cast<char *>(rdma_manager->local_mem_pool[0]->addr);
+  strcpy(msg, "message from computing node");
+  int msg_size = sizeof("message from computing node");
+  rdma_manager->RDMA_Write(rdma_manager->remote_mem_pool[0], &mem_pool_table[0],
+                           msg_size, thread_id);
+
+  rdma_manager->RDMA_Read(rdma_manager->remote_mem_pool[0], &mem_pool_table[1],
+                          msg_size, thread_id);
+  ibv_wc* wc = new ibv_wc();
+  while(wc->opcode != IBV_WC_RDMA_READ){
+    rdma_manager->poll_completion(wc, 2, thread_id);
+    if (wc->status != 0){
+      fprintf(stderr, "Work completion status is %d \n", wc->status);
+      fprintf(stderr, "Work completion opcode is %d \n", wc->opcode);
+    }
+
+  }
+  std::cout << "write buffer: " << (char*)mem_pool_table[0].addr << std::endl;
+
+  std::cout << "read buffer: " << (char*)mem_pool_table[1].addr << std::endl;
+
+}
+int main()
+{
+  struct config_t config = {
+      NULL,  /* dev_name */
+      NULL,  /* server_name */
+      19875, /* tcp_port */
+      1,	 /* ib_port */ //physical
+      -1, /* gid_idx */
+      4*10*1024*1024 /*initial local buffer size*/
+  };
+  auto Remote_Bitmap = new std::unordered_map<ibv_mr*, In_Use_Array>;
+  auto Local_Bitmap = new std::unordered_map<ibv_mr*, In_Use_Array>;
+  RDMA_Manager* rdma_manager = new RDMA_Manager(config, Remote_Bitmap, Local_Bitmap);
+//  RDMA_Manager rdma_manager(config, Remote_Bitmap, Local_Bitmap);
+  rdma_manager->Client_Set_Up_RDMA();
+  std::thread thread_object(client_thread, rdma_manager);
+
+  return 0;
+}
