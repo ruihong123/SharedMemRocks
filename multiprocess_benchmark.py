022ea36cb0154a1bc4eb91f058682e6caeadaff1
ruihong123 <wang4996@purdue.edu>
2021-05-24 23:20:21 -0400

    add python scripts

--- multiprocess_benchmark.py
+++ multiprocess_benchmark.py
@@ -9,7 +9,8 @@ TIMES_TO_RUN = 5
 # STUDENT_TEST_LOG_NAME = 'test-results'
 # STUDENT_SUBMISSION_DIR = "Assignment4"
 # SCORES = {}
-# SEARCH_TERMS = ['Tests passed', 'Points', 'Total time']
+SEARCH_TERMS = ['fillrandom']
+DB_BENCH_DIR = "./cmake-build-release"
 # LAB_NUMBER = 3
 # TIMEOUT = 10 * 60
 def run(cmd, out, cwd, timeout_sec):
@@ -35,4 +36,4 @@ for run_index in range(TIMES_TO_RUN):
                '--histogram=1', '--memtablerep=skip_list', '--bloom_bits=10', '--open_files=-1', '--max_background_compactions=16',
                '--max_write_buffer_number=8', '--max_background_flushes=7', '--level0_file_num_compaction_trigger=4', '--level0_stop_writes_trigger=20',
                '--level0_slowdown_writes_trigger=12', '--threads=16', '--allow_concurrent_memtable_write=true', '--disable_wal=1', '--seed=1606010580']
-        proc = subprocess.Popen(cmd, cwd="./release", stdout=out, stderr=out)
+        proc = subprocess.Popen(cmd, cwd=DB_BENCH_DIR, stdout=out, stderr=out)
---
+++ multiprocess_summary.py
@@ -0,0 +1,27 @@
+import argparse
+import json
+import os
+import subprocess
+from distutils.dir_util import copy_tree
+from shutil import copyfile, rmtree
+from threading import Timer
+TIMES_TO_RUN = 5
+# STUDENT_TEST_LOG_NAME = 'test-results'
+# STUDENT_SUBMISSION_DIR = "Assignment4"
+# SCORES = {}
+SEARCH_TERMS = ['fillrandom']
+DB_BENCH_DIR = "./cmake-build-release"
+# LAB_NUMBER = 3
+# TIMEOUT = 10 * 60
+total_bandwidth=0
+for run_index in range(TIMES_TO_RUN):
+    db_path = os.path.join(DB_BENCH_DIR + "/result"+ str(run_index) + '.txt')
+
+    with open(db_path, 'r') as out:
+        test_results = out.read()
+    for line in test_results.split('\n'):
+        for term in SEARCH_TERMS:
+            if term in line:
+                print("line split " + line.split()[6])
+                total_bandwidth = total_bandwidth + float(line.split()[6])
+print("total bandwidth is " + str(total_bandwidth))
