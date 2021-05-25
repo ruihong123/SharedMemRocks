bc4ff1b6a575f125bf49e301a223cc207926f20f
ruihong123 <wang4996@purdue.edu>
2021-05-25 00:20:32 -0400

    add python scripts

--- multiprocess_benchmark.py
+++ multiprocess_benchmark.py
@@ -31,7 +31,7 @@ for run_index in range(TIMES_TO_RUN):
                '--db=./temp' + str(run_index), '--wal_dir=./temp' + str(run_index), '--num=10000000', '--num_levels=6', '--key_size=16', '--value_size=400', '--block_size=8192', '--cache_size=6442450944',
                '--cache_numshardbits=6', '--compression_max_dict_bytes=0', '--compression_ratio=0.5', '--compression_type=none', '--level_compaction_dynamic_level_bytes=true', '--bytes_per_sync=8388608',
                '--cache_index_and_filter_blocks=0', '--pin_l0_filter_and_index_blocks_in_cache=1', '--benchmark_write_rate_limit=0',
-               '--hard_rate_limit=3', '--rate_limit_delay_max_milliseconds=1000000', '--write_buffer_size=134217728', '--target_file_size_base=134217728',
+               '--hard_rate_limit=3', '--rate_limit_delay_max_milliseconds=1000000', '--write_buffer_size=67108864', '--target_file_size_base=134217728',
                '--max_bytes_for_level_base=1073741824', '--verify_checksum=1', '--delete_obsolete_files_period_micros=62914560',
                '--max_bytes_for_level_multiplier=8', '--statistics=0', '--stats_per_interval=1', '--stats_interval_seconds=60',
                '--histogram=1', '--memtablerep=skip_list', '--bloom_bits=10', '--open_files=-1', '--max_background_compactions=1',
