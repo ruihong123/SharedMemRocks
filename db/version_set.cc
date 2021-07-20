4237efea589154a42e4aa86e544affe75c1c3c14
ruihong123 <wang4996@purdue.edu>
2021-07-19 23:45:48 -0400

    modify the random seed to make it the same with OurLSM

--- db/version_set.cc
+++ db/version_set.cc
@@ -1927,14 +1927,14 @@ void Version::Get(const ReadOptions& read_options, const LookupKey& k,
 
     *status = Status::NotFound(); // Use an empty error message for speed
   }
-//#ifdef GETANALYSIS
+#ifdef GETANALYSIS
   auto stop = std::chrono::high_resolution_clock::now();
   auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
 //  std::printf("Get from SSTables (not found) time elapse is %zu\n",  duration.count());
 //  printf("GetNum within version::get %lu\n", VersionSet::GetNum.load());
   VersionSet::GetTimeElapseSum.fetch_add(duration.count());
   VersionSet::GetNum.fetch_add(1);
-//#endif
+#endif
 }
 
 void Version::MultiGet(const ReadOptions& read_options, MultiGetRange* range,
