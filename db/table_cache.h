311449eb0be1b23c1bfbd34734e2edf7f79094e9
ruihong123 <wang4996@purdue.edu>
2021-07-27 09:09:04 -0400

    Fix RDMA cache allocator

--- db/table_cache.h
+++ db/table_cache.h
@@ -91,7 +91,20 @@ class TableCache {
       bool skip_filters, int level, size_t max_file_size_for_l0_meta_pin,
       const InternalKey* smallest_compaction_key,
       const InternalKey* largest_compaction_key, bool allow_unprepared_value);
-
+  static void CleanAll(){
+    GetTimeElapseSum = 0;
+    GetNum = 0;
+    filtered = 0;
+    not_filtered = 0;
+    DataBinarySearchTimeElapseSum = 0;
+    IndexBinarySearchTimeElapseSum = 0;
+    DataBlockFetchBeforeCacheElapseSum = 0;
+    foundNum = 0;
+    cache_hit_look_up_time = 0;
+    cache_miss_block_fetch_time = 0;
+    cache_hit = 0;
+    cache_miss = 0;
+  }
   // If a seek to internal key "k" in specified file finds an entry,
   // call get_context->SaveValue() repeatedly until
   // it returns false. As a side effect, it will insert the TableReader
