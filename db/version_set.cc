412d037b93983ab7687fd77160d6f29b7fe1b7c5
ruihong123 <wang4996@purdue.edu>
2021-10-08 13:51:12 -0400

    cmake for c8220

--- db/version_set.cc
+++ db/version_set.cc
@@ -3685,7 +3685,7 @@ VersionSet::~VersionSet() {
 #ifdef PROCESSANALYSIS
   printf("GetNum is %lu", VersionSet::GetNum.load());
   if (VersionSet::GetNum.load() >0)
-    printf("LSM Version GET time statics is %zu, %zu, %zu\n",
+    printf("LSM Version GET time statistics is %zu, %zu, %zu\n",
            VersionSet::GetTimeElapseSum.load(), VersionSet::GetNum.load(),
            VersionSet::GetTimeElapseSum.load()/VersionSet::GetNum.load());
 #endif
