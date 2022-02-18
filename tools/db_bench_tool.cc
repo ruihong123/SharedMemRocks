c7a7c09f5f8d25fa3f4d21a8ef530b6e2dcd13a3
ruihong123 <wang4996@purdue.edu>
2022-02-17 22:03:49 -0500

    reduce wait time

--- tools/db_bench_tool.cc
+++ tools/db_bench_tool.cc
@@ -3472,7 +3472,7 @@ class Benchmark {
     }
     delete[] arg;
     if (method == &Benchmark::WriteRandom)
-      sleep(700); // Digestion for SSTable compaction.
+      sleep(80); // Digestion for SSTable compaction.
     return merge_stats;
 
   }
