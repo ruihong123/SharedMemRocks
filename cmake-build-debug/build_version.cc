ae42e5196a2df729b1c4fb206e90ad5f8a7017a5
ruihong123 <wang4996@purdue.edu>
2022-09-28 19:58:14 -0400

    make no RDMA allocated buffer in cache, copy from RDMA buffer to the cache buffer evey block read.

--- cmake-build-debug/build_version.cc
+++ cmake-build-debug/build_version.cc
@@ -1,5 +1,5 @@
 // Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@18fd1f251f35e587924c221f65442249e573b9d1@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2022/02/09 23:56:32@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@5bc9a1edd00cc67b37e028a43ccfae477d9d4dcb@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2022/09/28 16:08:20@";
 const char* rocksdb_build_compile_date = __DATE__;
